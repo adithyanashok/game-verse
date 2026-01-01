4d5d9bebf82bef567cf05c27984b76b09b44150b
Adithyan Ashok <adithyanashok406@gmail.com>
2026-01-01 12:51:46 +0530

    chore: updated host

--- backend/libs/common/src/constants/microservice.constants.ts
+++ backend/libs/common/src/constants/microservice.constants.ts
@@ -1,38 +1,38 @@
-const getHost = (envVar: string, productionFallback: string) => {
-  return (
-    process.env[envVar] ||
-    (process.env.NODE_ENV === 'production' ? productionFallback : 'localhost')
-  );
-};
+// const getHost = (envVar: string, productionFallback: string) => {
+//   return (
+//     process.env[envVar] ||
+//     (process.env.NODE_ENV === 'production' ? productionFallback : 'localhost')
+//   );
+// };
 
 export const MICROSERVICE_CONFIG = {
   get GAME_SERVICE() {
     return {
-      host: getHost('GAME_HOST', 'game'),
+      host: 'game',
       port: 4000,
     };
   },
   get REVIEW_SERVICE() {
     return {
-      host: getHost('REVIEW_HOST', 'review'),
+      host: 'review',
       port: 5000,
     };
   },
   get AUTH_SERVICE() {
     return {
-      host: getHost('AUTH_HOST', 'auth'),
+      host: 'auth',
       port: 6000,
     };
   },
   get USER_SERVICE() {
     return {
-      host: getHost('USER_HOST', 'user'),
+      host: 'user',
       port: 7000,
     };
   },
   get DISCUSSION_SERVICE() {
     return {
-      host: getHost('DISCUSSION_HOST', 'discussion'),
+      host: 'discussion',
       port: 8000,
     };
   },
