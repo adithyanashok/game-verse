15eb8b2322a682f2a7fc7b12644aa1937d4c6dda
Adithyan Ashok <adithyanashok406@gmail.com>
2026-01-03 23:13:28 +0530

    chore: updated port

--- backend/apps/discussion/src/main.ts
+++ backend/apps/discussion/src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   app.connectMicroservice({
     transport: Transport.TCP,
     options: {
-      host: MICROSERVICE_CONFIG.DISCUSSION_SERVICE.host,
+      host: '0.0.0.0',
       port: MICROSERVICE_CONFIG.DISCUSSION_SERVICE.port,
     },
   });
