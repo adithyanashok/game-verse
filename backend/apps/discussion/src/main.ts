801c5386a4ae450837be25b0c963b1f4fd918b93
Adithyan Ashok <adithyanashok406@gmail.com>
2026-01-03 23:25:21 +0530

    fix: fixed discussion error

--- backend/apps/discussion/src/main.ts
+++ backend/apps/discussion/src/main.ts
@@ -1,20 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { DiscussionModule } from './discussion.module';
 import { Transport } from '@nestjs/microservices';
-import { MICROSERVICE_CONFIG } from 'libs/common/src';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 
 // import { SocketIoClientProvider } from './socket-io.provider';
 
 async function bootstrap() {
   const app = await NestFactory.create(DiscussionModule);
-  // const socketIoClientProvider = app.get<SocketIoClientProvider>(
-  //   SocketIoClientProvider,
-  // );
-
-  // app.connectMicroservice<MicroserviceOptions>({
-  //   strategy: new SocketIoClientStrategy(socketIoClientProvider.getSocket()),
-  // });
 
   app.useWebSocketAdapter(new IoAdapter(app));
 
@@ -22,14 +14,15 @@ async function bootstrap() {
     transport: Transport.TCP,
     options: {
       host: '0.0.0.0',
-      port: MICROSERVICE_CONFIG.DISCUSSION_SERVICE.port,
+      port: 8000,
     },
   });
 
   await app.startAllMicroservices();
 
-  const port = process.env.DISCUSSION_WS_PORT || 8000;
-  await app.listen(port, '0.0.0.0');
-  console.log(`Discussion WebSocket server running on port ${port}`);
+  const wsPort = process.env.DISCUSSION_WS_PORT || 8001;
+  await app.listen(wsPort, '0.0.0.0');
+
+  console.log(`Discussion WS running on port ${wsPort}`);
 }
 bootstrap();
